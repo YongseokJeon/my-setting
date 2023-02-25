7b036dcb754866f34059c3772003cc0509b62fff
Yongseok Jeon <ysjeon741@gmail.com>
2023-02-25 12:42:05 +0900

    Update my power shell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,3 +8,6 @@ Set-Alias -Name gco -Value git-checkout
 
 function git-log { git log }
 Set-Alias -Name gl -Value git-log
+
+function git-submodule-update { git submodule update $args }
+Set-Alias -Name gsu -Value git-submodule-update
